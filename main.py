59d21e94f59f9a159e6e50d7dd7e4f8cc1a4ab5c
jgriffitht <jgriffith@brooklynsteamcenter.org>
2025-12-21 01:25:27 -0500

    sec

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for, flash,abort
+from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
+
+
+import pymysql
+
+
+from dynaconf import Dynaconf
+
+
+app = Flask(__name__)
